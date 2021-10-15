ec9dcd0aa42794ff7a5e151925aa8906e0bb850f
Rudra <chaudhary66667@gmail.com>
2021-10-15 12:01:25 +0530

    Added states list

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from pprint import pprint
 import os
 from tkinter import *
+from tkinter import messagebox
 load_dotenv()
 
 PINK = "#7027A0"
@@ -11,7 +12,11 @@ GREEN = "#6ECB63"
 
 STATES_LIST = ['Andaman Nicobar islands','Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chandigarh','Chattisgarh','Delhi','Dadra & Nagar','Goa','Gujrat','Himachal Pradesh','Haryana','Jharkhand','Jammu & Kashmir','Karnataka','Kerla','Ladakh','Lakshadweep Islands','Mahrashtra','Meghalya','Manipur','Madhya Pradesh','Mizoram','Nagaland','Odisha','Punjab','Pondicherry','Rajasthan','Sikkim','Telangana','Tamil nadu','Tripura','Uttar Pradesh','Uttarakhand','West Bengal']
 WEBSITE_URL = f"{os.getenv('API_URL')}"
-print(WEBSITE_URL)
+
+STATES_LIST = [f"{states}\n" for states in STATES_LIST]
+def show_states():
+    messagebox.showinfo("List of States","".join(STATES_LIST))
+
 
 data = requests.get(url=WEBSITE_URL).json()
 
@@ -33,5 +38,7 @@ canvas = Canvas(width=500,height=500,highlightthickness=0,bg=GREEN)
 covid_image =  PhotoImage(file="covid.png")
 covid_canvas = canvas.create_image(250,250,image = covid_image)
 canvas.pack()
+show_list_button = Button(text="Show Availabel States",highlightthickness=0,bg=GREEN,command=show_states)
+show_list_button.pack()
 
 window.mainloop()
